33c5dbea2226059de76174ab55435e55aaafdd0f
jhamadhav <jhamadhav28@gmail.com>
2021-10-01 21:16:55 +0530

    raw -> participants data

---
+++ refineData/filterParticipantsData.js
@@ -0,0 +1,33 @@
+const db = require("./../dbFunc.js")
+let data = db.read("./refineData/rawParticipantsData.json")
+data = JSON.parse(data)
+
+// console.log(data);
+
+const monToNum = (inp) => {
+    return new Date(Date.parse(inp + " 1, 2012")).getMonth()
+}
+
+data = data.map((elem) => {
+    let time = elem["Enrolment Date & Time"]
+    // console.log(elem);
+    time = time.split(" ")
+    let month = Number(monToNum(time[1]))
+    let day = Number(time[2])
+    let year = Number(time[3])
+
+    return {
+        "name": elem["Student Name"],
+        "profileLink": elem["Qwiklabs Profile URL"],
+        // "email": elem["gcrstudentEmail"],
+        "enrollDate": Date.UTC(year, month, day),
+        "enrollStatus": elem["Enrolment Status"] == "All Good" ? true : false
+    }
+})
+
+// console.log(data[0]);
+
+let participants = {
+    "profiles": data
+}
+db.write("participants.json", participants)
